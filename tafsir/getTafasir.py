e77fc37308f7e6bac1cd14e39b342bc01c3b969f
shayba@shaybix.com <owner@owners-MacBook-Pro.local>
2014-06-28 05:03:22 +0100

    some more commit

--- tafsir/getTafasir.py
+++ tafsir/getTafasir.py
@@ -2,7 +2,9 @@ import urllib2
 from bs4 import BeautifulSoup
 import re, os
 
+global count
 bookUrl = 'http://shamela.ws/index.php/book/'
+bookBase = 'http://shamela.ws/browse.php/book-'
 
 # This function is called by the getUrl() function and is passed a url for the book page
 # and this code gets that page, and looks for url for the online shamela reading app in order
@@ -10,18 +12,25 @@ bookUrl = 'http://shamela.ws/index.php/book/'
 
 
 def getBook(bookUrl):
-    global count
-    while (count <= max ):
+    count = 0
+    pageCount = 1
+    file = open('index.txt', 'wb')
+    while (count < max ):
+        print max
+        print count
         url = bookUrls[count].split('/')
         bookId = url[-1].split('-')[-1]
-        print bookId
         pageUrl = url[0] + '//' + url[2] + '/' + url[3] +  '/book/' + 'get_page/' + bookId + '/'
         print pageUrl
-    #    while(urllib2.urlopen(pageUrl + str(count)).getcode() == 200):
-    #        page = urllib2.urlopen(pageUrl + str(count)).read()
-    #        print page
-    #        count += 1
-        count = count + 1
+        while(urllib2.urlopen(pageUrl + str(pageCount)).getcode() == 200):
+
+            page = urllib2.urlopen(pageUrl + str(pageCount)).read()
+            index = '\n{ "index" : { "_index" : "books", "_type" : "book_one", "_id" : "' + str(page) + '" } }\n' + str(page)
+            file.write('\n{ "index" : { "_index" : "books", "_type" : "book_one", "_id" : "' + str(page) + '" } }\n' + str(page))
+            print index
+
+            pageCount += 1
+        return 0
 	
 
 # This function is responsible for reading the urls that are stored in a text file.
@@ -29,11 +38,9 @@ def getBook(bookUrl):
 
 def getUrl():
     global data
-
     global bookUrls
     global max
     bookUrls = []
-    count = 0
     try:
 #		Reading from file that contains urls for the books
         f = open('url.txt', 'r')
@@ -43,14 +50,14 @@ def getUrl():
         for line in data:
 #			store regex pattern in page
             urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
-            url = urls[0]
-            authorPage = urllib2.urlopen(url).read()
-            links = re.findall('http://shamela.ws(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', authorPage)
-            bookUrl = links[2]
-            print bookUrl
+#            url = urls[0]
+#            authorPage = urllib2.urlopen(url).read()
+#            links = re.findall('http://shamela.ws(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', authorPage)
+#            bookUrl = links[2]
+            url = urls[0].split('/')
+            bookUrl = bookBase + url[-1]
             bookUrls.append(bookUrl)
         max = len(bookUrls)
-        print max            
         getBook(bookUrls)
         
            
