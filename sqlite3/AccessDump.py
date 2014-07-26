73a6c4cb3a37d6e602d76fc05067da635c7feae9
shaybix <shayba@shaybix.com>
2014-07-26 06:28:11 +0100

    Added AccessDump.py as it is a dependency for the extract.py script

---
+++ sqlite3/AccessDump.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# AccessDump.py
+# A simple script to dump the contents of a Microsoft Access Database.
+# It depends upon the mdbtools suite:
+#   http://sourceforge.net/projects/mdbtools/
+# Website:
+#   http://www.guyrutenberg.com/2012/07/16/sql-dump-for-ms-access-databases-mdb-files-on-linux/
+# MDB_JET3_CHARSET="cp1256" python AccessDump.py filename.bok | sqlite3 filename.mfth
+
+import sys, subprocess, os
+ 
+DATABASE = sys.argv[1]
+ 
+# Dump the schema for the DB
+subprocess.call(["mdb-schema", DATABASE, "mysql"])
+ 
+# Get the list of table names with "mdb-tables"
+table_names = subprocess.Popen(["mdb-tables", "-1", DATABASE],
+                               stdout=subprocess.PIPE).communicate()[0]
+tables = table_names.splitlines()
+ 
+print "BEGIN;" # start a transaction, speeds things up when importing
+sys.stdout.flush()
+ 
+# Dump each table as a CSV file using "mdb-export",
+# converting " " in table names to "_" for the CSV filenames.
+for table in tables:
+    if table != '':
+        subprocess.call(["mdb-export", "-I", "mysql", DATABASE, table])
+ 
+print "COMMIT;" # end the transaction
+sys.stdout.flush()
