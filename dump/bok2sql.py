83baf64dfc7ca637f19322f8a70f98bd3f6aa57b
shaybix <shayba@shaybix.com>
2015-07-03 22:20:00 +0100

    initiated new directory called dump for dumping all the data

---
+++ dump/bok2sql.py
@@ -0,0 +1,47 @@
+import sqlite3
+import os
+import sys
+import time
+
+
+
+class DB:
+
+    def __init__(self):
+
+        pass
+
+
+
+
+    def init(self):
+        pass
+
+
+
+
+    def save(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    pass
