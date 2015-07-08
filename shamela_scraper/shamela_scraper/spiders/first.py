415a150eca3c408fd73079c8050af4dd2866f223
shaybix <shayba@shaybix.com>
2015-07-08 02:37:34 +0100

    updates

--- shamela_scraper/shamela_scraper/spiders/first.py
+++ shamela_scraper/shamela_scraper/spiders/first.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class FirstSpider(scrapy.Spider):
+class FirstSpider(CrawlSpider):
     name = "first"
-    allowed_domains = ["shamela.ws"]
-    start_urls = ('http://www.shamela.ws/')
+    allowed_domains = ['shamela.ws']
+    start_urls = ['http://www.shamela.ws/', 'http://www.shamela.ws/index.php/categories']
+
+    rules = [
+            Rule(
+                LinkExtractor(
+                    #allow=['http:\/\/www\.shamela\.ws\/index.php\/book\/[0-9]*$']
+                    allow=['.*'],
+                    deny=['http:\/\/www\.shamela\.ws\/index.php\/book\/get_pdf\/[0-9]*$', 'http:\/\/www\.shamela\.ws\/browse.php\/.*$']
+                    ),
+                follow=True
+                
+                )
+            ]
+
+#    def parse(self, response):
+
+#         pass
 
 
-    def parse(self, response):
-
-        pass
