8fa3732f63fbd18e42bfd6d3bea4d71873f3f832
shaybix <shayba@shaybix.com>
2015-07-08 03:42:56 +0100

    updates

--- shamela_scraper/shamela_scraper/spiders/first.py
+++ shamela_scraper/shamela_scraper/spiders/first.py
@@ -6,22 +6,22 @@ from scrapy.linkextractors import LinkExtractor
 class FirstSpider(CrawlSpider):
     name = "first"
     allowed_domains = ['shamela.ws']
-    start_urls = ['http://www.shamela.ws/', 'http://www.shamela.ws/index.php/categories']
+    start_urls = ['http://www.shamela.ws/index.php/categories']
 
     rules = [
             Rule(
                 LinkExtractor(
                     #allow=['http:\/\/www\.shamela\.ws\/index.php\/book\/[0-9]*$']
-                    allow=['.*'],
-                    deny=['http:\/\/www\.shamela\.ws\/index.php\/book\/get_pdf\/[0-9]*$', 'http:\/\/www\.shamela\.ws\/browse.php\/.*$']
+                    allow=['.*']
+                    #deny=['http:\/\/www\.shamela\.ws\/index.php\/book\/get_pdf\/[0-9]*$', 'http:\/\/www\.shamela\.ws\/browse.php\/.*$']
                     ),
-                follow=True
+                follow=False
                 
                 )
             ]
 
-#    def parse(self, response):
+    #def parse(self, response):
 
-#         pass
+         #pass
 
 
